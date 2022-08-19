cfc56199fc2c886fc4e687dcb880b738290a2e04
alswns3906 <alswns3906@naver.com>
2022-08-19 13:25:42 +0900

    Book1 추가

--- Book1/src/Ch03/P88.java
+++ Book1/src/Ch03/P88.java
@@ -1,5 +1,20 @@
 package Ch03;
 
 public class P88 {
+	public static void main(String[] args) {
+		int myAge = 23;
+		int teacherAge = 38;
+		
+		boolean value = (myAge > 25);
+		System.out.println(value);
+		
+		System.out.println(myAge <= 25);
+		System.out.println(myAge != teacherAge);
+		
+		char ch;
+		ch = (myAge > teacherAge) ? 'T' : 'F';
+		System.out.println(ch);
+
+	}
 
 }
