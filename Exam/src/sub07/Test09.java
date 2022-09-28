f7aadc1fde463e0bcbf7b957e6cdfe30e25fa8b2
alswns3906 <alswns3906@naver.com>
2022-09-28 10:49:32 +0900

    2022/09/28

---
+++ Exam/src/sub07/Test09.java
@@ -0,0 +1,32 @@
+package sub07;
+
+/*
+ * 날짜 : 2022/09/28
+ * 이름 : 홍민준
+ * 내용 : 자바 총정리 연습문제
+ */
+abstract class Tv{
+	public abstract void turnOn();
+	public abstract void turnOff();
+}
+
+
+public class Test09 {
+	public static void main(String[] args) {
+		Tv tv = new Tv() {
+			public void turnOn() {
+				System.out.println("Tv를 켭니다.");
+			}
+			
+			public void turnOff() {
+				System.out.println("Tv를 끕니다.");
+			}
+		};
+		
+		tv.turnOn();
+		tv.turnOff();
+		
+		
+	}
+
+}
